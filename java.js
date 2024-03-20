726f123c08c90ea6c1c2628c202afb9fa6387a95
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-03-20 21:02:51 +0100

    Decided for the size of the parent div Adding the width & height as const number of container div Adding the class for each div inside the function to loop

--- java.js
+++ java.js
@@ -1,14 +1,16 @@
 const gridContainer = document.querySelector('#gridContainer')
+const sizeSquare = 520;
+let userInput = 2;
+let result = sizeSquare/userInput;
 
 
 function addGrid() {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add('gridSquare');
+    gridDiv.setAttribute(`style`, `width: ${result}px; height: ${result}px`);
     gridContainer.appendChild(gridDiv);
 }
 
-
-for(let i = 0; i < 256; i++) {
-    addGrid();
+for(let i = 0; i < userInput*userInput; i++) {
+     addGrid();
 }
-
