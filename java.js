14f13eb97b5bd759b034e513328ea937bee0d82f
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-03-22 21:28:46 +0100

    Finished the main structure and added the button Finishing the logic of the project with problems to solve Adding the function to the click event for the user input Getting the value of the user input and putting inside the function/onclick

--- java.js
+++ java.js
@@ -1,16 +1,41 @@
 const gridContainer = document.querySelector('#gridContainer')
+const btn = document.querySelector('#btn')
 const sizeSquare = 520;
-let userInput = 2;
-let result = sizeSquare/userInput;
+const standardSquare = 16;
+const result = sizeSquare/standardSquare;
 
 
-function addGrid() {
+for(let i = 0; i < standardSquare**2 ; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add('gridSquare');
     gridDiv.setAttribute(`style`, `width: ${result}px; height: ${result}px`);
     gridContainer.appendChild(gridDiv);
+    const hovering = document.querySelectorAll('.gridSquare');
+    hovering[i].addEventListener('mouseover', () => {
+        hovering[i].setAttribute('class', 'gridHover')
+    });
 }
 
-for(let i = 0; i < userInput*userInput; i++) {
-     addGrid();
-}
+let userInput = "";
+
+btn.addEventListener('click', () => {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    userInput = prompt("Type a number between 1 and 100")
+    let newResult = sizeSquare/userInput;
+    if (userInput > 1 || userInput < 100) {
+        for(let i = 0; i < userInput**2 ; i++) {
+            const gridDiv = document.createElement("div");
+            gridDiv.classList.add('gridSquare');
+            gridDiv.setAttribute(`style`, `width: ${newResult}px; height: ${newResult}px`);
+            gridContainer.appendChild(gridDiv);
+            const hovering = document.querySelectorAll('.gridSquare');
+            hovering[i].addEventListener('mouseover', () => {
+                hovering[i].setAttribute('class', 'gridHover')
+            });
+        }
+    } else {
+        alert("Please enter a Number between 1 and 100")
+    }
+});
